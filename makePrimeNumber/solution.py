3fc4e20a2455ca46061ed99d6c813a56eb16f872
SaenaKIM <tosk3927@gmail.com>
2022-04-18 19:12:34 +0900

    [TIL] makePrimeNumber

---
+++ makePrimeNumber/solution.py
@@ -0,0 +1,20 @@
+from itertools import combinations
+
+def solution(nums):
+    answer = 0
+    list = [i for i in combinations(nums, 3)]
+    for set in list:
+        s = sum(set)
+        isPrime = True;
+        for n in range(2,s):
+            if s % n == 0:
+                isPrime = False
+                break
+        if isPrime:
+            answer += 1
+
+    return answer
+
+
+#print(solution([1,2,3,4]))
+#print(solution([1,2,7,6,4]))
