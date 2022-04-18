d9aa988646f5ef281a80da2481d14b936a22e6fe
SaenaKIM <tosk3927@gmail.com>
2022-04-18 18:31:53 +0900

    convert print statement to comment

--- addNumberNotInList/solution.py
+++ addNumberNotInList/solution.py
@@ -1,3 +1,5 @@
+#https://programmers.co.kr/learn/courses/30/lessons/86051
+
 def solution(numbers):
     list = [0,1,2,3,4,5,6,7,8,9]
     for num in numbers:
@@ -6,4 +8,4 @@ def solution(numbers):
     answer = sum(list)
     return answer
 
-print(solution([1,2,3,4,6,7,8,0]))
+#print(solution([1,2,3,4,6,7,8,0])) #14
