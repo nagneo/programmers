bf2e3afadef9b8f3fe5ada5c9222abdc0bcef240
SaenaKIM <tosk3927@gmail.com>
2022-04-18 18:23:32 +0900

    [TIL] add Number which is not in the list

---
+++ addNumberNotInList/solution.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    list = [0,1,2,3,4,5,6,7,8,9]
+    for num in numbers:
+        list.remove(num)
+    
+    answer = sum(list)
+    return answer
+
+print(solution([1,2,3,4,6,7,8,0]))
